a4286796fd4564642b9d630480874ef693c8c442
HyeJeong Yoon <youne@korea.ac.kr>
2025-06-08 17:50:46 +0900

    로컬 베이스 url 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/designsystem/',
-  build: {
-    outDir: 'dist',
-  },
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+  
+  return {
+    plugins: [react()],
+    base: isProduction ? '/designsystem/' : '/',
+    build: {
+      outDir: 'dist',
+    },
+    // 추가로 개발 서버 설정도 가능
+    server: {
+      port: 5173,
+      open: true, // 자동으로 브라우저 열기
+    }
+  }
 })
